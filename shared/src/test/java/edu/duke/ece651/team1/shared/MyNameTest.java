8dfd314378e4abd1461844897c53a5115a485673
ht175 <ht175@duke.edu>
2024-03-15 01:31:52 -0400

    correct shared

--- shared/src/test/java/edu/duke/ece651/team1/shared/MyNameTest.java
+++ shared/src/test/java/edu/duke/ece651/team1/shared/MyNameTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class MyNameTest {
   @Test
   public void test_getName() {
-    assertEquals("teamX", MyName.getName());
+    assertEquals("team1", MyName.getName());
   }
 
 }
