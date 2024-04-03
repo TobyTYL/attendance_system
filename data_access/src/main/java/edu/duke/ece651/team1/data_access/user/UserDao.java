7f6b6348e5c1420c7d0d18cdc3667b8af0cba653
TobyTYL <tobytaott@gmail.com>
2024-04-02 23:35:25 -0400

    Resolved conflict in settings.gradle

---
+++ data_access/src/main/java/edu/duke/ece651/team1/data_access/user/UserDao.java
@@ -0,0 +1,10 @@
+package edu.duke.ece651.team1.dao.user;
+import java.util.List;
+
+public interface UserDao {
+    void addUser(User user);
+    void removeUser(int userId);
+    void updateUser(User user);
+    User getUserById(int userId);
+    List<User> getAllUsers();
+}
