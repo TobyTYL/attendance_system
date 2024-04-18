5ac0d339c7478331bc04bd95df67afd32e38e128
TobyTYL <tobytaott@gmail.com>
2024-04-18 00:49:06 -0400

    update student view page

--- JavaFX_Test/src/main/java/edu/duke/ece651/team1/javafx_test/HelloController.java
+++ JavaFX_Test/src/main/java/edu/duke/ece651/team1/javafx_test/HelloController.java
@@ -1,14 +1,41 @@
 package edu.duke.ece651.team1.javafx_test;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    @FXML
+    private AnchorPane manageStudentButton;
+//    @FXML
+//    private Label welcomeText;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+
+    private void manageStudentButtonClicked (ActionEvent event){
+        try {
+            FXMLLoader student_loader = new FXMLLoader(getClass().getResource("student-view.fxml"));
+            Parent root = student_loader.load();
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) manageStudentButton.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
