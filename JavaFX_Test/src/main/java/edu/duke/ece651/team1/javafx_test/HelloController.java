0f9b5c5cd2b41c50f1aca288c7636646790b4201
TobyTYL <tobytaott@gmail.com>
2024-04-18 15:31:50 -0400

    update professor view

--- JavaFX_Test/src/main/java/edu/duke/ece651/team1/javafx_test/HelloController.java
+++ JavaFX_Test/src/main/java/edu/duke/ece651/team1/javafx_test/HelloController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -15,27 +16,38 @@ public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML
-    private AnchorPane manageStudentButton;
-//    @FXML
-//    private Label welcomeText;
+    private Button button_manage_student;
+    @FXML
+    private Button button_manage_professor;
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
     @FXML
-
-    private void manageStudentButtonClicked (ActionEvent event){
+    private void manageStudentButtonClicked(ActionEvent event) {
         try {
-            FXMLLoader student_loader = new FXMLLoader(getClass().getResource("student-view.fxml"));
-            Parent root = student_loader.load();
+            FXMLLoader studentLoader = new FXMLLoader(getClass().getResource("student-view.fxml"));
+            Parent root = studentLoader.load();
             Scene scene = new Scene(root);
-
-            Stage stage = (Stage) manageStudentButton.getScene().getWindow();
+            Stage stage = (Stage) button_manage_student.getScene().getWindow();
             stage.setScene(scene);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
 
+
+    @FXML
+    private void manageProfessorButtonClicked (ActionEvent event){
+        try {
+            FXMLLoader professor_loader = new FXMLLoader(getClass().getResource("professor-view.fxml"));
+            Parent root = professor_loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) button_manage_professor.getScene().getWindow();
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
