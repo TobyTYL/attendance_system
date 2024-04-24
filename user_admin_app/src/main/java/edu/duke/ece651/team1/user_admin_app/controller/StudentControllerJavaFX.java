76c619b24604b31a228e8bbf37ed94bf1a1c655f
TobyTYL <tobytaott@gmail.com>
2024-04-24 01:34:33 -0400

    add student done

--- user_admin_app/src/main/java/edu/duke/ece651/team1/user_admin_app/controller/StudentControllerJavaFX.java
+++ user_admin_app/src/main/java/edu/duke/ece651/team1/user_admin_app/controller/StudentControllerJavaFX.java
@@ -1,18 +1,32 @@
 package edu.duke.ece651.team1.user_admin_app.controller;
 
 import edu.duke.ece651.team1.data_access.Student.StudentDaoImp;
+import edu.duke.ece651.team1.shared.Student;
+import edu.duke.ece651.team1.shared.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import edu.duke.ece651.team1.data_access.User.UserDao;
+import edu.duke.ece651.team1.data_access.User.UserDaoImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 import java.io.IOException;
 
+import static java.lang.System.out;
+
 public class StudentControllerJavaFX {
     private StudentDaoImp studentDao = new StudentDaoImp();
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private UserDao userDao = new UserDaoImp();
 
     @FXML
     private Button button_add_student;
@@ -24,12 +38,12 @@ public class StudentControllerJavaFX {
     private Button button_back_to_student_view;
     @FXML
     private Button button_back_to_main_view;
-//    @FXML
-//    private TextField studentLegalName;
-//    @FXML
-//    private TextField studentDisplayName;
-//    @FXML
-//    private TextField studentEmail;
+    @FXML
+    private TextField studentLegalName;
+    @FXML
+    private TextField studentDisplayName;
+    @FXML
+    private TextField studentEmail;
     @FXML
     private void addStudentButtonClicked(ActionEvent event) {
         try {
@@ -38,13 +52,25 @@ public class StudentControllerJavaFX {
             Scene scene = new Scene(root);
             Stage stage = (Stage) button_add_student.getScene().getWindow();
             stage.setScene(scene);
-//            String legalName = studentLegalName.getText();
-//            String displayName = studentDisplayName.getText();
-//            String email = studentEmail.getText();
-//            saveStudentToDatabase(legalName, displayName, email);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void confirmButtonClicked(ActionEvent event) {
+        String legalName = studentLegalName.getText();
+        String displayName = studentDisplayName.getText();
+        String email = studentEmail.getText();
+        User user = new User(legalName, passwordEncoder.encode("passw0rd"), "Student");
+        int uid = userDao.addUser(user);
+        Student student = new Student(uid, legalName, displayName, email);
+//        Student student = new Student( legalName, displayName, email);
+
+        studentDao.addStudent(student);
+        out.println("Student added successfully.");
     }
     @FXML
     private void removeStudentButtonClicked(ActionEvent event) {
