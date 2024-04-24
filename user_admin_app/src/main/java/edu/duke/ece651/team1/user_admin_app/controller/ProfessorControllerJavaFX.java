637fabc3b2f3bc5639bb54099f7e873a33881fa7
TobyTYL <tobytaott@gmail.com>
2024-04-24 11:24:20 -0400

    add & remove professor done

--- user_admin_app/src/main/java/edu/duke/ece651/team1/user_admin_app/controller/ProfessorControllerJavaFX.java
+++ user_admin_app/src/main/java/edu/duke/ece651/team1/user_admin_app/controller/ProfessorControllerJavaFX.java
@@ -1,15 +1,31 @@
 package edu.duke.ece651.team1.user_admin_app.controller;
+import edu.duke.ece651.team1.data_access.Professor.ProfessorDao;
+import edu.duke.ece651.team1.data_access.Professor.ProfessorDaoImp;
+import edu.duke.ece651.team1.data_access.User.UserDao;
+import edu.duke.ece651.team1.data_access.User.UserDaoImp;
+import edu.duke.ece651.team1.shared.Professor;
+import edu.duke.ece651.team1.shared.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import static java.lang.System.out;
+
 public class ProfessorControllerJavaFX {
+    private ProfessorDao professorDao = new ProfessorDaoImp();
+    private UserDao userDao = new UserDaoImp();
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+
     @FXML
     private Button button_add_professor;
     @FXML
@@ -18,6 +34,10 @@ public class ProfessorControllerJavaFX {
     private Button button_back_to_professor_view;
     @FXML
     private Button button_back_to_main_view;
+    @FXML
+    private TextField add_professor_name;
+    @FXML
+    private TextField remove_professor_name;
     @FXML
     private void addProfessorButtonClicked(ActionEvent event) {
         try {
@@ -31,6 +51,18 @@ public class ProfessorControllerJavaFX {
         }
     }
     @FXML
+    private void addProfessorConfirmButtonClicked(ActionEvent event) {
+        String professorName = add_professor_name.getText();
+        User user = new User(professorName, passwordEncoder.encode("passw0rd"), "Professor");
+        int userId = userDao.addUser(user);
+        Professor newProfessor = new Professor(userId, professorName);
+        if (professorDao.checkProfessorExists(professorName)) {
+            out.println("Professor Already exists!");
+            return;
+        }
+        professorDao.addProfessor(newProfessor);
+        out.println("Professor added successfully.");    }
+    @FXML
     private void removeProfessorButtonClicked(ActionEvent event) {
         try {
             FXMLLoader professorLoader = new FXMLLoader(getClass().getResource("/edu.duke.ece651.team1.user_admin_app/Professor/remove-professor-view.fxml"));
@@ -40,6 +72,19 @@ public class ProfessorControllerJavaFX {
             stage.setScene(scene);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    @FXML
+    private void removeProfessorConfirmButtonClicked(ActionEvent event) {
+        String professorName = remove_professor_name.getText();
+        int userId = userDao.findUserByUsername(professorName).getUserId();
+        if (userId != -1) {
+            int professorId = professorDao.findProfessorByUsrID(userId).getProfessorId();
+            professorDao.removeProfessor(professorId);
+            userDao.removeUser(userId);
+            out.println("Professor removed successfully.");
+        } else {
+            out.println("Professor not found.");
         }
     }
     @FXML
