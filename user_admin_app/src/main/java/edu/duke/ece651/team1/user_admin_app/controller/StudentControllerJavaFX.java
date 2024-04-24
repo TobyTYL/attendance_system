e1482e8929277aee0ec4c69c72c1be5fc2763b79
TobyTYL <tobytaott@gmail.com>
2024-04-24 11:10:34 -0400

    update student & remove student done

--- user_admin_app/src/main/java/edu/duke/ece651/team1/user_admin_app/controller/StudentControllerJavaFX.java
+++ user_admin_app/src/main/java/edu/duke/ece651/team1/user_admin_app/controller/StudentControllerJavaFX.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 import java.io.IOException;
+import java.util.Optional;
 
 import static java.lang.System.out;
 
@@ -39,11 +40,19 @@ public class StudentControllerJavaFX {
     @FXML
     private Button button_back_to_main_view;
     @FXML
-    private TextField studentLegalName;
+    private TextField add_student_legalName;
     @FXML
-    private TextField studentDisplayName;
+    private TextField add_student_display_name;
     @FXML
-    private TextField studentEmail;
+    private TextField add_student_email;
+    @FXML
+    private TextField remove_student_name;
+    @FXML
+    private TextField update_student_legalName;
+    @FXML
+    private TextField update_student_display_name;
+    @FXML
+    private TextField update_student_email;
     @FXML
     private void addStudentButtonClicked(ActionEvent event) {
         try {
@@ -60,15 +69,13 @@ public class StudentControllerJavaFX {
     }
 
     @FXML
-    private void confirmButtonClicked(ActionEvent event) {
-        String legalName = studentLegalName.getText();
-        String displayName = studentDisplayName.getText();
-        String email = studentEmail.getText();
+    private void addConfirmButtonClicked(ActionEvent event) {
+        String legalName = add_student_legalName.getText();
+        String displayName = add_student_display_name.getText();
+        String email = add_student_email.getText();
         User user = new User(legalName, passwordEncoder.encode("passw0rd"), "Student");
         int uid = userDao.addUser(user);
         Student student = new Student(uid, legalName, displayName, email);
-//        Student student = new Student( legalName, displayName, email);
-
         studentDao.addStudent(student);
         out.println("Student added successfully.");
     }
@@ -85,6 +92,24 @@ public class StudentControllerJavaFX {
         }
     }
     @FXML
+    private void removeConfirmButtonClicked(ActionEvent event) {
+        String legalName = remove_student_name.getText();
+        Optional<Student> optionalStudent = studentDao.findStudentByName(legalName);
+        if (optionalStudent.isPresent()) {
+            Student student = optionalStudent.get();
+            Integer studentId = student.getStudentId();
+            if (studentId != null) {
+                studentDao.removeStudent(student);
+                userDao.removeUser(student.getUserId());
+                out.println("Student removed successfully.");
+            } else {
+                out.println("Student ID is null.");
+            }
+        } else {
+            out.println("Student not found.");
+        }
+    }
+    @FXML
     private void updateStudentButtonClicked(ActionEvent event) {
         try {
             FXMLLoader studentLoader = new FXMLLoader(getClass().getResource("/edu.duke.ece651.team1.user_admin_app/Student/update-student-view.fxml"));
@@ -94,6 +119,27 @@ public class StudentControllerJavaFX {
             stage.setScene(scene);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    @FXML
+    private void updateConfirmButtonClicked(ActionEvent event) {
+        String legalName = update_student_legalName.getText();
+        Optional<Student> optionalStudent = studentDao.findStudentByName(legalName);
+        if (optionalStudent.isPresent()) {
+            Student student = optionalStudent.get();
+            Integer studentId = student.getStudentId();
+            if (studentId != null) {
+                String newDisplayName = update_student_display_name.getText();
+                String newEmail = update_student_email.getText();
+                student.updateDisplayName(newDisplayName);
+                student.setStudentEmail(newEmail);
+                studentDao.updateStudent(student);
+                out.println("Student updated successfully.");
+            } else {
+                out.println("Student ID is null.");
+            }
+        } else {
+            out.println("Student not found.");
         }
     }
     @FXML
