141b0e4a314e27446627841a01e9e72bb3cf14cf
TobyTYL <tobytaott@gmail.com>
2024-04-29 21:07:31 -0400

    update

--- enrollmentApp_javafx/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module edu.duke.ece651.team1.enrollmentApp_javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires edu.duke.ece651.team1.shared;
-    requires edu.duke.ece651.team1.data_access;
-
-    opens edu.duke.ece651.team1.enrollmentApp_javafx.enrollmentApp_javafx to javafx.fxml;
-    exports edu.duke.ece651.team1.enrollmentApp_javafx.enrollmentApp_javafx;
-}
--- useradmin_javafx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.duke.ece651.team1.useradmin_javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens edu.duke.ece651.team1.useradmin_javafx to javafx.fxml;
-    exports edu.duke.ece651.team1.useradmin_javafx;
-}
