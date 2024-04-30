3f668c0362a8465b68d29f1f2a2abf975d45da23
TobyTYL <tobytaott@gmail.com>
2024-04-29 21:09:39 -0400

    update without client and server

--- JavaFX_Test/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.duke.ece651.team1.javafx_test {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens edu.duke.ece651.team1.javafx_test to javafx.fxml;
-    exports edu.duke.ece651.team1.javafx_test;
-}
